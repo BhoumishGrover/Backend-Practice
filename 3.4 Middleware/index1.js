08ef33dfc9daee361de4e5a075c0f4da76edb15f
BhoumishGrover <bhoumish@gmail.com>
2025-04-11 15:52:14 +0530

    added nodemon

--- 3.4 Middleware/index1.js
+++ 3.4 Middleware/index1.js
@@ -7,13 +7,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", (req, res) => {
   console.log(__dirname + "/public/index.html");
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit",(req,res)=>{
-  app.use(bodyParser.urlencoded({extended:true}));
   console.log(req.body);
   res.send("<h1>Data was replaced</h1>");
 });
--- 3.4 Middleware/index2.js
+++ 3.4 Middleware/index2.js
@@ -1,11 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname+"/public/index.html");
