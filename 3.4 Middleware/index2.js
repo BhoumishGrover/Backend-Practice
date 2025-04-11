9caa5799e6482c1c247f2c28fbd68c7c1f06da69
BhoumishGrover <bhoumish@gmail.com>
2025-04-11 16:02:16 +0530

    added morgan parser

--- 3.4 Middleware/index2.js
+++ 3.4 Middleware/index2.js
@@ -1,21 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import nodemon from "nodemon";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-
 app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname+"/public/index.html");
-  console.log(req.body);
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/submit",(req,res) => {
-  app.use(bodyParser.urlencoded({extended:true}));
-  res.send("<h1>Data has been recieved</h1>");
-  res.send(req.body);
-  console.log(req.body);
+app.post("/submit",(req,res)=>{
+  console.log(res.morgan);
+  res.send("Data stored")
 });
 
 app.listen(port, () => {
