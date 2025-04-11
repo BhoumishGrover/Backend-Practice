08e1f3fc5bf753d18b5b0ea50289e4af39f72871
BhoumishGrover <bhoumish@gmail.com>
2025-04-11 14:06:37 +0530

    sending data through post

--- 3.4 Middleware/index1.js
+++ 3.4 Middleware/index1.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -9,6 +10,12 @@ const port = 3000;
 app.get("/", (req, res) => {
   console.log(__dirname + "/public/index.html");
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit",(req,res)=>{
+  app.use(bodyParser.urlencoded({extended:true}));
+  console.log(req.body);
+  res.send("<h1>Data was replaced</h1>");
 });
 
 app.listen(port, () => {
--- 3.4 Middleware/index2.js
+++ 3.4 Middleware/index2.js
@@ -1,10 +1,22 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.sendFile(__dirname+"/public/index.html");
+  console.log(req.body);
+});
+
+app.post("/submit",(req,res) => {
+  app.use(bodyParser.urlencoded({extended:true}));
+  res.send("<h1>Data has been recieved</h1>");
+  res.send(req.body);
+  console.log(req.body);
 });
 
 app.listen(port, () => {
